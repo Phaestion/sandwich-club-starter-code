855619ee29c5975a4413555104a1395e67bd33bf
Steyn Geldenhuys <steyn@truevolve.technology>
2018-08-07 19:00:34 +0000

    Upgraded gradle and Sdk, also parsing json

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,51 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
+
+        try {
+            JSONObject job = new JSONObject(json);
+
+            JSONObject nameJob = job.getJSONObject("name");
+            String mainName = nameJob.getString("mainName");
+            List<String> akaList = jarrToArrayList(nameJob.getJSONArray("alsoKnownAs"));
+
+            String placeOfOrigin = job.getString("placeOfOrigin");
+            String description = job.getString("description");
+            String image = job.getString("image");
+
+            List<String> ingredientsList = jarrToArrayList(nameJob.getJSONArray("ingredients"));
+
+            return new Sandwich(mainName,
+                    akaList,
+                    placeOfOrigin,
+                    description,
+                    image,
+                    ingredientsList);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         return null;
+    }
+
+    private static ArrayList<String> jarrToArrayList(JSONArray jarr) throws JSONException {
+        ArrayList<String> arrayList = new ArrayList<String>();
+
+        for (int i = 0; i < jarr.length(); i++) {
+            arrayList.add(jarr.getString(i));
+        }
+
+        return arrayList;
     }
 }
